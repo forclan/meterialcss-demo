87ae77c5b5085637496c9d588b7e29928ef68bea
forclan <hxplian1992@163.com>
2016-07-30 09:15:26 +0800

    use gulp-concat to merge scss->css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
     gulp.src('src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(concat('build.css'))
         .pipe(gulp.dest('./src/css/'));
+
 });
 
 //Watch task
